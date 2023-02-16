811de6b6912d460e30910a1b70dd366fff22c8b1
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-16 18:57:31 +0100

    Add express and dotenv npm packages and set server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-const a = "EsLint dejame subir este commit porfa";
+import "./loadEnviroment.js";
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT ?? 4000;
+
+const server = app.listen(port);
+
+app.get("/", (req, res) => {
+  res.json({ pong: true });
+});
