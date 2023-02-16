d8547bcd54d80bc4c0123c1c3f227cf99eb495ba
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-16 21:55:27 +0100

    Create controller for get data by id

--- src/thingsController/thingsController.ts
+++ src/thingsController/thingsController.ts
@@ -2,5 +2,17 @@ import { type Request, type Response } from "express";
 import thingsIknow from "../data/data.js";
 
 export const getThings = (req: Request, res: Response) => {
+  if (!thingsIknow) {
+    res.status(400).json({});
+    return;
+  }
+
   res.status(200).json({ thingsIknow });
+};
+
+export const getThingById = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const thingById = thingsIknow.find((thing) => thing.id === +id);
+  res.status(200).json({ thingById });
 };
